2830f1574f88a523f84c36c56d4cf762d26e5bc9
Baelyk <b43lyk@gmail.com>
2018-07-21 21:33:37 -0500

    Completed
    Not actually, but in theory you can now launch DF with this.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate cursive;
 
 use cursive::views::{Dialog, TextView};
 use cursive::Cursive;
+use std::process::Command;
 
 fn main() {
     // Creates the cursive root - required for every application
@@ -11,9 +12,19 @@ fn main() {
     cursive.add_layer(
         Dialog::around(TextView::new("Hello World!"))
             .title("Cursed DF Launcher")
-            // .button("Launch", |c| c.quit())
+            .button("Launch", |c| {
+                launch_df();
+                c.quit();
+            })
             .button("Quit", |c| c.quit()),
     );
 
     cursive.run();
+}
+
+fn launch_df() {
+    Command::new("./df")
+        .current_dir("df/df_osx/")
+        .spawn()
+        .expect("The attempt to launch DF failed.");
 }
