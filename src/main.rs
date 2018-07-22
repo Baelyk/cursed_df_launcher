730d693666f0411bc66f89761c8bf447718601df
Baelyk <b43lyk@gmail.com>
2018-07-21 19:47:55 -0500

    Added Cursive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate cursive;
+
+use cursive::views::{Dialog, TextView};
+use cursive::Cursive;
+
 fn main() {
-    println!("Hello, world!");
+    // Creates the cursive root - required for every application
+    let mut cursive = Cursive::new();
+
+    // Creates a dialog with a single "Quit" button
+    cursive.add_layer(
+        Dialog::around(TextView::new("Hello World!"))
+            .title("Cursed DF Launcher")
+            // .button("Launch", |c| c.quit())
+            .button("Quit", |c| c.quit()),
+    );
+
+    cursive.run();
 }
